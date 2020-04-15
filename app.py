b144ae083046ce068c57fd2be7b8d67c89795210
Marco Bellido <marco.bellido@letsgoi.com>
2020-04-15 17:26:10 +0200

    feature: notify witch product/s or quantities are removed from cart

--- app.py
+++ app.py
@@ -8,24 +8,24 @@ def get_merchants_to_check():
     print('Merchants to check:\n')
 
     print('[0] All')
-    print('\n'.join(['[' + str(index + 1) + '] ' + merchant.get('name') for index, merchant in enumerate(config.primenow_merchants)]))
+    print('\n'.join(['[' + str(index + 1) + '] ' + merchant['name'] for index, merchant in enumerate(config.primenow_merchants)]))
 
     merchants_to_check = int(input('\nEnter selection [0 - ' + str(len(config.primenow_merchants)) + '] (0): ') or '0')
 
-    if merchants_to_check < 0 or merchants_to_check > (len(config.primenow_merchants) + 1):
-        raise Exception('Error on select merchants to check')
+    assert 0 <= merchants_to_check <= len(config.primenow_merchants), 'Error on select merchants to check'
 
-    return merchants_to_check
+    return [
+        PrimeNowChecker(merchant)
+        for index, merchant in enumerate(config.primenow_merchants)
+        if merchants_to_check == 0 or merchants_to_check == (index + 1)
+    ]
 
 
 if __name__ == '__main__':
-    primenow_checker = PrimeNowChecker()
-
-    merchants_to_check = get_merchants_to_check()
+    merchants_checkers = get_merchants_to_check()
     seconds_between_requests = int(input('Set seconds between requests (60): ') or '60')
 
     while True:
-        for index, merchant in enumerate(config.primenow_merchants):
-            if merchants_to_check == 0 or merchants_to_check == (index + 1):
-                primenow_checker.check_merchant(merchant)
-                time.sleep(seconds_between_requests)
+        for merchant_checker in merchants_checkers:
+            merchant_checker.check()
+            time.sleep(seconds_between_requests)
